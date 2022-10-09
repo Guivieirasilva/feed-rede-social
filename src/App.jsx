9569cd5ffdd747717508df26ea7dd85f3018ff1e
gui vieira <guivieirasilva02@gmail.com>
2022-10-09 11:44:08 -0300

    implementando formulário

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import './App.css'
+import '../src/styles/App.css'
+
+import UserIcon from "./images/user.svg"
+import PaperPlaneIcon from "./images/paper-plane.svg"
+
+import './styles/PostForm.css'
 
 function App() {
-  
 
   return (
     <div className="wrapper">
-      <h1> Hello World</h1>
+      <form onSubmit={() => alert('Formulário submetido')} className='post-form'>
+        <input placeholder='Escreva um nova história...'/>
+
+        <div>
+          <img src={UserIcon} alt='Ícone de usuário' />
+
+          <input placeholder='Digite seu nome...'/>
+
+        <button type='submit'>
+          <img src={PaperPlaneIcon} alt='Ícone de aviãozinho' />
+          Publicar
+        </button>
+
+        </div>
+      </form>
     </div>
   )
 }
